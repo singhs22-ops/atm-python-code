63fd3e418328207bff02fa3c5f28aef116fd4b90
singhs22-ops <65162678+singhs22-ops@users.noreply.github.com>
2020-05-18 15:20:34 +0530

    Add files via upload
    Additional file changes required

--- filehandling.py
+++ filehandling.py
@@ -6,7 +6,7 @@ print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 def getword_a(test_str,n):
 
     # printing original string
-    print("Original_String : " + test_str)
+    #print("Original_String : " + test_str)
 
     # initializing N
     N = n
@@ -31,34 +31,56 @@ def getword_a(test_str,n):
 
 print(os.getcwd())
 f = open("student.txt","w")
-text ="qwerty"
+
 f.writelines("101 Santosh maths 35\n")
 f.writelines("101 Santosh Hindi 30\n")
-f.writelines("102 Hina English 41")
-f.writelines(text)
-file_txt =open("student.txt","r")
-d= dict()
-tot = dict()
+f.writelines("102 Hina English 41\n")
+n = int(input("Enter the number of data to be entered:"))
+for i in range(n):
+    text = input("Add remaning contents")
+    f.writelines(text)
+    f.write("\n")
+
+f.close()
+total_marks= dict()
+name = dict()
 over = dict()
+arr_final =""
+dict_filen = dict()
+file_txt =open("student.txt","r+")
+
 for line in file_txt:
 
-    res1 =line
+    res1 = line
+    print("lines", line)
     res=res1[:3]
     word1 = getword_a(res1,4)
-    if (res in d):
-        d[res] = d[res]+int(word1)
+    if (res in total_marks):
+        total_marks[res] = total_marks[res]+int(word1)
         #total marks
+        print("totalmarks::",total_marks[res])
+        arr_final = over[res] + res1
+
+        over.pop(res)
+        over[res] = arr_final
+
 
     else:
-        d[res] = int(word1)
-        tot[res] = getword_a(res1,2)
+        total_marks[res] = int(word1)
+        name[res] = getword_a(res1,2)
         over[res] = res1
 
-        print("word",tot )
-        strt = tot[res]+"."+res+".txt"
-        fnew =open(strt,"w")
-        fnew.write(res1)
-        fnew.write("Total ")
-        fnew.write(str(d[res]))
+
+        dict_filen[res] = name[res]+"."+res+".txt"
+        fnew =open(dict_filen[res],"w")
+
+
+
+for result in total_marks:
+    fileher = open(dict_filen[result], "w")
+    fileher.write(over[result])
+    fileher.write("\t\t\tTotal ")
+    fileher.write(str(total_marks[result]))
+    print(result)
 
 file_txt.close()
